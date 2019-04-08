c8d7eff93b870ff119939cab4fbf13886520425a
edabruzzo <emmanuel.oliveira3@gmail.com>
2019-04-08 20:50:58 -0300

    Cria a pagina principal e a pasta javascript

---
+++ public/javascripts/index.js
@@ -0,0 +1,11 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#valor'),
+    document.querySelector('#quantidade')
+];
+
+console.log(campos);
+
+document.querySelector('.form').addEventListener('submit', function(event) {
+    alert('oi');
+  });
